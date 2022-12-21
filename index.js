4ee4e337a6e1136a5a3385201c1aa596fde63c0f
Souvik Chatterjee <Souvik.chat2011@gmail.com>
2022-12-21 10:52:40 +0530

    added transition to hamburger

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("Script running....")
-//document.querySelector('.cross').style.display = 'none';
+document.querySelector('.cross').style.display = 'none';
 document.querySelector('.hamburger').addEventListener("click",()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
     
