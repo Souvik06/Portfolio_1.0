a0e5f25d71bcb53a9500a5bdce576ed65c855511
Souvik Chatterjee <Souvik.chat2011@gmail.com>
2022-12-21 10:43:14 +0530

    updated hamberger functionality

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log("Script running....")
 document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    
+})
